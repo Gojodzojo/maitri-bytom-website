a168d43ddd20aae526f162a63341d83acb6ab145
Mateusz Goik <mateuszgoik1@wp.pl>
2023-03-06 18:37:22 +0100

    Replace index_proxy with cms_server

--- cms_server/src/main.rs
+++ cms_server/src/main.rs
@@ -1,15 +1,61 @@
-use rocket::fs::FileServer;
-use std::env::current_dir;
+use rocket::{
+    http::ContentType,
+    response::{self, Responder},
+    Request, Response,
+};
+use std::{io::Cursor, str::FromStr};
+
+struct Res {
+    text: String,
+    content_type: Option<String>,
+}
+
+impl<'a> Responder<'a, 'static> for Res {
+    fn respond_to(self, _: &Request) -> response::Result<'static> {
+        let content_type = match self.content_type {
+            Some(ct) => ContentType::from_str(&ct).unwrap(),
+            None => ContentType::Plain,
+        };
+
+        Response::build()
+            .header(content_type)
+            .sized_body(self.text.len(), Cursor::new(self.text))
+            .ok()
+    }
+}
+
+async fn from_url(url: &str) -> Option<Res> {
+    let resp = reqwest::get(url).await.ok()?;
+    let headers = resp.headers();
+    let content_type = match headers.get("Content-Type") {
+        Some(ct) => Some(ct.to_str().unwrap().to_string()),
+        None => None,
+    };
+    let text = resp.text().await.ok()?;
+
+    Some(Res { content_type, text })
+}
+
+#[rocket::get("/")]
+async fn index() -> Option<Res> {
+    let url = "https://raw.githubusercontent.com/Gojodzojo/maitri-bytom-website/main/netlify_cms/index_github_resources.html";
+    let mut x = from_url(url).await;
+
+    match &mut x {
+        Some(x) => x.content_type = Some("text/html".to_string()),
+        None => {}
+    };
+
+    x
+}
 
 #[rocket::get("/proxy?<path>")]
-async fn proxy(path: String) -> Option<String> {
-    reqwest::get(path).await.ok()?.text().await.ok()
+async fn proxy(path: String) -> Option<Res> {
+    from_url(&path).await
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
     println!("Wejdź na stronę localhost:8000");
-    rocket::build()
-        .mount("/", rocket::routes![proxy])
-        .mount("/", FileServer::from(current_dir().unwrap()))
+    rocket::build().mount("/", rocket::routes![proxy, index])
 }
